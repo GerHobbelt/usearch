a5fbb24f5b6c5d08cf7b829be1b804b6750b9b87
Gurgen Yegoryan <21982202+gurgenyegoryan@users.noreply.github.com>
2023-08-29 09:21:22 +0000

    Make: Add rm file

--- usearch.rb
+++ usearch.rb
@@ -10,14 +10,11 @@ class Usearch < Formula
   sha256 "5e89f631d39a7db7f46d87d634904c8c93ae1d08a7fd787d3076a945ff32e4a4"
 
   def install
-    system "wget https://github.com/gurgenyegoryan/usearch/releases/download/v0.1.1/usearch_macOS_1.2.0.zip"
-    # Extract the archive.
-    system "unzip usearch_macOS_1.2.0.zip"
+    # Create a libexec directory and extract the archive there
+    libexec.install Dir["*"]
 
-    # Move the `lib.a` file to `/usr/local/lib`.
-    system "mv libusearch_c.a /usr/local/lib/libusearch_c.a"
-
-    # Move the `usearch.h` file to `/usr/local/include`.
-    system "mv usearch.h /usr/local/include/"
+    # Create symlinks to the necessary files in the Cellar's include and lib directories
+    include.install_symlink Dir["#{libexec}/usearch.h"]
+    lib.install_symlink Dir["#{libexec}/libusearch_c.a"]
   end
 end
